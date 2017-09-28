94cea5ec8fe17c541492ec9d8c2f1733e41aab48
Vladimir Rodchenko <vladimir@aurochses.com>
2017-09-28 13:36:20 +0300

    initial code

---
+++ test/Aurochses.Module.Data.Autofac.Tests/DataModuleBaseTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Aurochses.Module.Data.Autofac.Tests
+{
+    public class DataModuleBaseTests
+    {
+        [Fact]
+        public void Inherit_Module()
+        {
+            // Arrange & Act
+            var dataModuleBase = new DataModuleBase();
+
+            // Assert
+            Assert.IsAssignableFrom<global::Autofac.Module>(dataModuleBase);
+        }
+    }
+}
